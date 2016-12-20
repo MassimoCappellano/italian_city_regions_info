d33e36db47f334eb0920b16f29893f87faba77b0
Massimo Cappellano <Massimo.Cappe@gmail.com>
2016-12-20 14:38:27 +0100

    'improvements'

---
+++ map_operations.js
@@ -0,0 +1,116 @@
+'use strict';
+
+const Promise = require("bluebird");
+
+const level = require('level');
+const path 	= require('path');
+
+const db = level(path.join(__dirname,'/db'));
+
+var ac = {};
+
+ac.getElencoRegioni = function (resolve, reject) {
+
+	return new Promise( function (resolve, reject){
+	  let codesRegions = [];
+	  let key = 'regioni:';
+
+	  db.createReadStream({ start: key, end: key + '\xff' })
+	    .on('data', function (data) {
+	      codesRegions.push(data);
+	    })
+	    .on('end', function () {
+	      resolve(codesRegions);
+	    });
+	}).then( function (results) {
+
+		return results.map( (item) => {
+			item.key = item.key.replace('regioni:', '');
+			return item;
+		});
+	});
+};
+
+/*
+	return arr objs:	{
+			name: name,
+			provincia_id: key provincia,
+			code: code,
+			regione_id: key regione
+		}
+*/
+
+ac.getProvinceByCodeRegione = function (codRegione) {
+
+	return new Promise( function (resolve, reject){
+	  let arrProv = [];
+	  let key = 'inv:province:';
+
+	  db.createReadStream({ start: key, end: key + '\xff' })
+	    .on('data', function (data) {
+	      arrProv.push(data);
+	    })
+	    .on('end', function () {
+	      resolve(arrProv);
+	    });
+	}).then( function (results) {
+		return results.map( (item) => {
+			let objProv = JSON.parse(item.value);
+			objProv.provincia_id = item.key.replace('inv:province:', '');
+			return objProv;
+		});
+	}).then( function (arrObjProv) {
+		return arrObjProv.filter( (objProv) => {
+			if(objProv.regione_id == codRegione)
+				return true;
+
+			return false;
+		});
+	});
+};
+
+/*
+	return arr objs: {
+		name: name,
+		comune_id, key comune,
+		provincia_id: provincia_id,
+		altitudine: altitudine,
+		superficie: superficie,
+		popolazione: popolazione
+	}
+
+*/
+
+ac.getComuniByCodeProvincia = function (codProvincia) {
+
+	return new Promise( function (resolve, reject){
+	  let arrMunicipality = [];
+	  let key = 'inv:comuni:';
+
+	  db.createReadStream({ start: key, end: key + '\xff' })
+	    .on('data', function (data) {
+	      arrMunicipality.push(data);
+	    })
+	    .on('end', function () {
+	      resolve(arrMunicipality);
+	    });
+	}).then( function (results) {
+		return results.map( (item) => {
+			let objMun = JSON.parse(item.value);
+			objMun.comune_id = item.key.replace('inv:comuni:', '');
+			return objMun;
+		});
+	}).then( function (arrObjMun) {
+		return arrObjMun.filter( (objMun) => {
+			if(objMun.provincia_id == codProvincia)
+				return true;
+
+			return false;
+		});
+	});
+}
+
+
+
+
+module.exports = ac;
---
+++ test/map_operations.test.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const mo = require('../map_operations');
+
+const test = require('tape');
+
+// for disable tests
+function xtest(){
+
+}
+
+xtest('find elenco regioni', function(t) {
+	mo.getElencoRegioni().done(
+			function (results) {
+				console.log(results);
+			},
+			function (error) {
+				console.log(error);
+			}
+		);
+
+	t.end();
+});
+
+xtest('find elenco province \'Lombardia\' code: 3', function(t) {
+	mo.getProvinceByCodeRegione(3).done(
+		function (results) {
+				console.log(results);
+			},
+		function (error) {
+				console.log(error);
+			}
+		);
+
+	t.end();
+});
+
+
+xtest('find elenco province by invalid code regione: 5555', function(t) {
+	mo.getProvinceByCodeRegione(5555).done(
+		function (results) {
+				console.log(results);
+			},
+		function (error) {
+				console.log(error);
+			}
+		);
+	
+	t.end();
+});
+
+
+test('find elenco comuni by code provincia: 12, Varese', function(t) {
+	mo.getComuniByCodeProvincia(12).done(
+		function (results) {
+				console.log(results);
+			},
+		function (error) {
+				console.log(error);
+			}
+		);
+	
+	t.end();
+});
+
+
+
